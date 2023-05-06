9fc8671a2584e0b3a9490c92abd71841d6e489b8
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-05-06 13:47:56 -0300

    generate invoice usecase

---
+++ src/modules/invoice/usecase/generate-invoice/generate-invoice.usecase.ts
@@ -0,0 +1,43 @@
+/* eslint-disable no-useless-constructor */
+import { Invoice } from '../../domain/invoice.entity'
+import InvoiceGateway from '../../gateway/invoice.gateway'
+import { GenerateInvoiceUseCaseInputDto } from './generate-invoice.dto'
+import Product from '../../domain/product.entity'
+import Id from '../../../@shared/domain/value-object/id.value-object'
+
+export default class GenerateInvoiceUseCase {
+  constructor(private _invoiceRepository: InvoiceGateway) {}
+
+  async execute(input: GenerateInvoiceUseCaseInputDto) {
+    const products = input.items.map((item) => {
+      return new Product({
+        id: new Id(item.id),
+        name: item.name,
+        price: item.price,
+      })
+    })
+
+    const invoice = new Invoice({
+      id: new Id(),
+      name: input.name,
+      document: input.document,
+      address: input.address,
+      items: products,
+    })
+
+    await this._invoiceRepository.save(invoice)
+
+    return {
+      id: invoice.id.id,
+      name: invoice.name,
+      document: invoice.document,
+      address: invoice.address,
+      items: invoice.items.map((item) => ({
+        id: item.id.id,
+        name: item.name,
+        price: item.price,
+      })),
+      total: invoice.total,
+    }
+  }
+}
