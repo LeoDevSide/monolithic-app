1459852c6df3f8a8eeec8cbe0ece3037e8231adf
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-05-06 13:28:55 -0300

    generate dto

---
+++ src/modules/invoice/usecase/generate-invoice/generate-invoice.dto.ts
@@ -0,0 +1,25 @@
+export interface GenerateInvoiceUseCaseInputDto {
+  name: string
+  document: string
+  address: string
+  items: {
+    id: string
+    name: string
+    price: number
+  }[]
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+export interface GenerateInvoiceUseCaseOutputDto {
+  id: string
+  name: string
+  document: string
+  address: string
+  items: {
+    id: string
+    name: string
+    price: number
+  }[]
+  total: number
+}
